5e3f1fc7b19d5b8e511d0b4d0d520e46e5184435
Skyler Saunders <156210299+SkySaunders@users.noreply.github.com>
2024-04-28 19:34:00 -0500

    Add files via upload

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,69 +1,87 @@
 $( document ).ready(function() {
     
-    // var input = document.getElementById('userText').value.toUpperCase();
-    // var output = document.getElementById('usrOutput');
+  // var input = document.getElementById('userText').value.toUpperCase();
+  // var output = document.getElementById('usrOutput');
 
-    var phoneticAlfa ={
-        "A":"Alfa",
-        "B":"Bravo",
-        "C":"Charlie",
-        "D":"Delta",
-        "E":"Echo",
-        "F":"Foxtrot",
-        "G":"Golf",
-        "H":"Hotel",
-        "I":"India",
-        "J":"Juliett",
-        "K":"Kilo",
-        "L":"Lima",
-        "M":"Mike",
-        "N":"November",
-        "O":"Oscar",
-        "P":"Papa",
-        "Q":"Quebec",
-        "R":"Romeo",
-        "S":"Sierra",
-        "T":"Tango",
-        "U":"Uniform",
-        "V":"Victor",
-        "W":"Whiskey",
-        "X":"Xray",
-        "Y":"Yankee",
-        "Z":"Zulu"
-    };
+  var phoneticAlfa ={
+      "A":"Alfa",
+      "B":"Bravo",
+      "C":"Charlie",
+      "D":"Delta",
+      "E":"Echo",
+      "F":"Foxtrot",
+      "G":"Golf",
+      "H":"Hotel",
+      "I":"India",
+      "J":"Juliett",
+      "K":"Kilo",
+      "L":"Lima",
+      "M":"Mike",
+      "N":"November",
+      "O":"Oscar",
+      "P":"Papa",
+      "Q":"Quebec",
+      "R":"Romeo",
+      "S":"Sierra",
+      "T":"Tango",
+      "U":"Uniform",
+      "V":"Victor",
+      "W":"Whiskey",
+      "X":"Xray",
+      "Y":"Yankee",
+      "Z":"Zulu"
+  };
 
-    var result =""; 
-    // Grab input from textbox
-    //split up user text into an array
-    //declare string as specific array to loop through
+  var result =""; 
+  // Grab input from textbox
+  //split up user text into an array
+  //declare string as specific array to loop through
 
-    
+  
 
 $("#submitButton").click(function () {
-        //compare value. with a stored object...
+      //compare value. with a stored object...
 
-        let userText = $('#userText').val().toUpperCase().split("");
+      let userText = $('#userText').val().toUpperCase().split("");
 
-        $.each(userText, (index, value)=> {
-            console.log(index, value);
+      $.each(userText, (index, value)=> {
+          console.log(index, value);
+          var letter = ''; //initialize aas empty
+          
+         // for (var i=0; i < userText.length; i++){
+          
+              letter = userText[index];
 
-            for (var i=0; i < userText.length; i++){
-            var letter = userText[i];
-            if(phoneticAlfa[letter]){
+              console.log('letter var = ', letter); //check what we got from array
+              
+              console.log('are we grabbing correct thing from our data thingy?', phoneticAlfa[letter]); 
+              
               result += phoneticAlfa[letter] + ' ';
-                break
-            }
-            else{
-              result += letter + ' (Not in NATO alphabet) ';
-            }
-          }
-          $('#textOutput').text(result);
-          });
-        });
-    });
+              console.log('result', result);
+
+          // if(phoneticAlfa[letter]){
+
+          //     console.log('are we grabbing correct thing from our data thingy?', phoneticAlfa.letter); 
+              
+          //   result += phoneticAlfa[letter] + ' ';
+          //     break
+          // }
+          // else{
+          //   result += letter + ' (Not in NATO alphabet) ';
+          // }
+       // }
+        
+        }); //closes .each()   
+
+       $('#textOutput').text(result);
+
+      });
+      
 
 
 $('#clear').click(function () {
-    $("#userText").val("")
-    $('#textOutput').val("")
+  $("#userText").val("")
+  $('#textOutput').val("")
+});
+
 });
